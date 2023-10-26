ab137ec33809bff97d1b4cf0abc2b88fbebd3210
Kevin Villamizar <kvillamizar196@gmail.com>
2023-10-26 16:39:45 -0500

    Tarjetas con borde añadidas

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -41,7 +41,9 @@ class _CardsVIew extends StatelessWidget {
       child: Column(
         children: [
           ...cards.map((card) =>
-              _CardType1(label: card['label'], elevation: card['elevation']))
+              _CardType1(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType2(label: card['label'], elevation: card['elevation'])),
         ],
       ),
     );
@@ -70,6 +72,39 @@ class _CardType1 extends StatelessWidget {
           Align(
             alignment: Alignment.bottomLeft,
             child: Text(label),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      elevation: elevation,
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(color: colors.outline)),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(children: [
+          Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              )),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('$label - outline'),
           )
         ]),
       ),
