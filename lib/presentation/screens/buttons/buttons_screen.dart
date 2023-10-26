6ebd5c29bcd9eb6abd9e9cb1cebebef8b8adf1b0
Kevin Villamizar <kvillamizar196@gmail.com>
2023-10-26 15:58:03 -0500

    Botón personalizado añadido

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -74,9 +74,33 @@ class _ButtonsView extends StatelessWidget {
               style: ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(colors.primary),
                   iconColor: const MaterialStatePropertyAll(Colors.white))),
-
-          //TODO: Custom Button
+          const CustomButton(),
         ]),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Mi botón',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
       ),
     );
   }
