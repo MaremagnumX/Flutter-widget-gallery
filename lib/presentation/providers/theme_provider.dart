69b63916dd06de93b67c183ab453adbf6ec5f750
Kevin Villamizar <kvillamizar196@gmail.com>
2023-10-28 16:17:29 -0500

    Implementación de Riverpod en CounterScreen

---
+++ lib/presentation/providers/theme_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final isDarkModePrvider = StateProvider<bool>((ref) => false);
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = "counter-screen";
@@ -9,9 +10,19 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int counter = ref.watch(counterProvider);
+    final bool isDark = ref.watch(isDarkModePrvider);
     final colors = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(
+                isDark ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModePrvider.notifier).state = !isDark;
+            },
+          )
+        ],
         title: const Text('Counter Screen'),
         titleTextStyle: TextStyle(
           color: colors.primary,
@@ -26,8 +37,13 @@ class CounterScreen extends ConsumerWidget {
           style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+          ref.read(counterProvider.notifier).state++;
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
